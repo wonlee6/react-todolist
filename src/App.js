0c398c16922ad6343c4788d426b496c030bb34cc
wonlee6 <wonlee6@gmail.com>
2021-04-28 16:22:51 +0900

    component UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import TodoCreate from './components/TodoCreate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -10,7 +14,11 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <div>hihi</div>
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
     </>
   );
 };
